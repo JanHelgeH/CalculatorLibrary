7df2ebcd987f717aceda97468540ff3cf7cd82cd
Jan Helge Høyland <jan.helge.hoyland@sparebank1.no>
2019-03-18 10:09:22 +0100

    Add function for addition and subtraction

---
+++ calculator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 18 10:00:57 2019
+
+@author: a9981
+"""
+
+"""
+Calculator library containing basic math operations.
+"""
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+def subtract(first_term, second_term):
+    return first_term - second_term
